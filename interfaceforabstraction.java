3eb6ab8999c76c9233b3653ccbbe5d51b362bb48
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-20 00:50:16 +0530

    Update interfaceforabstraction.java

--- interfaceforabstraction.java
+++ interfaceforabstraction.java
@@ -5,4 +5,30 @@ interface Tasks
     void play();
     void study();
   }
+class Human implemets Tasks
+  {
+    void sleep()
+      {
+        System.out.println("Ghumiyechhi");
+      }
+    void eat()
+      {
+        System.out.println("Khachhi");
+      }
+    void sleep()
+      {
+        System.out.println("Khelchhi");
+      }
+    void sleep()
+      {
+        System.out.println("Porchhi");
+      }
+  }
+class Maineta
+  {
+    Human manush = new Human();
+    mansuh.sleep();
+    manush.eat();
+  }
+    
 
