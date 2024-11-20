2d1e600f005c1f79c6ebd4aa6a2c75bfc31cc9cf
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-21 00:30:14 +0530

    Update interfacemultipleineritance.java

--- interfacemultipleineritance.java
+++ interfacemultipleineritance.java
@@ -1,1 +1,15 @@
+interface Flyable {
+    void fly();
+}
+interface Swimmable {
+    void swim();
+}
+class Duck implements Flyable, Swimmable {
+    public void fly() {
+        System.out.println("Duck is flying");
+    }
+    public void swim() {
+        System.out.println("Duck is swimming");
+    }
+}
 
